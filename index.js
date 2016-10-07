2c155102c907c5005a4a4961bec4cdec9e74e85e
Jarek Machaj <czemuja@gmail.com>
2016-10-07 16:17:50 +0200

    fix with undefined value

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 module.exports =
     function (dateFrom, dateTo){
-        var dif = dateFrom.getTime() - dateTo.getTime();
-        var seconds = Math.abs(dif / 1000);
+        var seconds = -1;
+        if (dateFrom != undefined && dateTo != undefined){
+            var dif = dateFrom.getTime() - dateTo.getTime();
+            seconds = Math.abs(dif / 1000);        
+        }
+
         return {
             seconds : seconds,
             minutes :  seconds / 60,
             hours : seconds / 3600,
             days : seconds / (3600 * 24)
         }
-    } 
+    }    
